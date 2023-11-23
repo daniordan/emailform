221a11b3c6081cf9d2be595cf64dc3260e15bc15
mariusdanbmd <34275284+mariusdanbmd@users.noreply.github.com>
2023-11-23 16:45:57 +0200

    initial implementation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,18 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'emailform';
+  email: string = '';
+
+  // constructor() {
+  //   setInterval(() => {
+  //     this.email += 'a';
+  //   }, 1000);
+  // }
+
+  onSubmit() {
+    console.log(this.email);
+  }
 }
